8340437c45dcb0bb5fa8c8b347507b6afd88a9a8
llJTOll <eodeoddl315@naver.com>
2022-01-27 05:50:51 +0900

    Create Challenge 수정1
    1. test.css 변수명 변경했습니다.
    2. text input components 추가했습니다.
    3. hr 가로줄 안보이던거 수정했습니다.
    4. 가로 중앙정렬 했습니다.

--- src/pages/CreateChallenge.svelte
+++ src/pages/CreateChallenge.svelte
@@ -1,5 +1,8 @@
 <script>
 import Button from '../components/Button.svelte';
+import Inputxt from '../components/Inputxt.svelte'; 
+import Container from '../components/Container.svelte';
+import "../common/colorVariable.css";
 import "../common/text.css";
 
 let challengename='';
@@ -12,85 +15,94 @@ function checkBlank(challengename){
 		alert(challengename + "이 생성되었습니다."); 
 }
 </script>
+<div class="Page">
+	<div class="Content">
+		<div class=title_text>Create New Challenge</div>
+		<hr align=left class=hr />
+		<div class=Sub_content>
+			<div class=subtitle_text>challenge name
+				<span style="color:red;">*</span>
+			</div>
+			<Inputxt bind:value={challengename} maxlength=20 size=20/>
+			
+			<div class=subtitle_text>Description</div>
 
-<div class="Grid">
-	<div class=--title-text>Create New Challenge</div>
-
-	<div class=sub_grid>
-		<div class=--subtitle-text>challenge name
-			<span style="color:red;">*</span>
+			<Inputxt bind:value={Description} size=80/>
 		</div>
-		<input type="text" bind:value={challengename} maxlength=20 size=20>
-		
-		<div class=--subtitle-text>Description</div>
+		<hr align=left class=hr />
+		<div class=Sub_content>
+			<div class=Contain>
+				<input type="radio" name="secure"checked="check">
+				<img class="Image" src="images/public.png" alt="public_img" />
+				<div>
+					<div class=common_text>Public</div>
+					<div class=explain_text>Anyone on the internet can see this Challenge!</div>
+				</div>
+			</div>
 
-		<input type="text" bind:value={Description} size=80>
-	</div>
-
-	<div class=sub_grid>
-		<div class=contain>
-			<input type="radio" name="secure"checked="check">
-			<img class="logo" src="images/public.png" alt="public_img" />
-			<div>
-				<div class=--common-text>Public</div>
-				<div class=--explain-text>Anyone on the internet can see this Challenge!</div>
+			<div class=Contain>
+				<input type="radio" name="secure" align="middle">
+				<img class="Image" src="images/private.png" alt="private_img" />
+				<div>
+					<div class=common_text>Private</div>
+					<div class=explain_text>You choose who can see and join to this Challenge!</div>
+				</div>
 			</div>
 		</div>
+		<hr align=left class=hr />
+			<!-- on click은 이후에 수정하기 (지금 작동x)-->	
+		<div class=Sub_content>
+			<Button on:click={() => checkBlank(challengename)} width='7rem' height='2.5rem' backgroundColor='var(--main-green-color)'>
+				<div class=button_text>Create</div>
+			</Button>
 
-		<div class=contain>
-			<input type="radio" name="secure">
-			<img class="logo" src="images/private.png" alt="private_img" />
-			<div>
-				<div class=--common-text>Private</div>
-				<div class=--explain-text>You choose who can see and join to this Challenge!</div>
-			</div>
+			<Button on:click={() =>{}}  width='7rem' height='2.5rem' backgroundColor='#E3E3E3'> 
+				<div class=button_text>Cancel</div>
+			</Button>
 		</div>
-	</div>
-
-
-		<!-- on click은 이후에 수정하기 (지금 작동x)-->	
-	<div class=sub_grid>
-		<Button on:click={() => checkBlank(challengename)} width='7rem' height='2.5rem' backgroundColor='var(--main-green-color)'>
-			<div class=--button-text>Create</div>
-		</Button>
-
-		<Button on:click={() =>{}}  width='7rem' height='2.5rem' backgroundColor='#E3E3E3'> 
-			<div class=--button-text>Cancle</div>
-		</Button>
 	</div>
 </div>
 
+
+
 <style lang="scss">
-
-	input[type=text]{
-			height: 1.9rem;
-			border: solid 1.2px 'var(--line-gray-color)';
-			border-radius: 10px;
-
-	}
-	input:focus{
-		outline:none;
-	}
-	.logo{
-		height: 100%;
-		margin-left:0.5rem;
-		margin-right:0.5rem;
-	}
-	.Grid{
-		display : flex;
-		padding-left:20%;
-		padding-right:20%;
+	.Page{
+		display: flex;
 		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 1rem;
 	}
-	.sub_grid{
+
+	.Content{
+		weight: auto;
+		height: auto;
+	}
+
+	.Sub_content{
 		margin-top:0.1rem;
-		margin-bottom:2rem;
 	}
-	.contain{
+
+	.Contain{
 		height: 2.5rem;
 		display: flex;
 		flex-direction: row;
 		margin-bottom: 0.5rem;
 	}
 
+	.Image{
+		height: 100%;
+		margin-left:0.5rem;
+		margin-right:0.5rem;
+	}
+
+	.hr{
+		border:none;
+		height: 1px;
+		width: 40rem;
+		background: var(--line-gray-color);
+		margin-top: 2rem;
+		margin-bottom: 2rem;
+		flex: 0 0 100%;
+	}
 </style>
