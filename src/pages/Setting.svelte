6a847d5aa67cb2e2d22c56603f7374e4ab64d4cf
MOBUMIN <kiju23@naver.com>
2022-01-28 22:23:37 +0900

    feat(setting): setting페이지 기본 틀 작성

--- src/pages/Setting.svelte
+++ src/pages/Setting.svelte
@@ -1,50 +1,12 @@
 <script>
-	import { push } from 'svelte-spa-router'
-	import { login } from '../store/user';
-	import Button from '../components/Button.svelte';
-	
-	function onClick() {
-		push('/')
-		login();
-	}
 </script>
 
-<div class="Login">
-	<img class="Login__logo" src="images/grabit_logo.png" alt="logo" />
-	<Button
-		width='15rem'
-		height='2.5rem'
-		backgroundColor='var(--main-white-color)'
-		{onClick}
-	>
-		<span class="Login__slot">
-			<img class="Login__icon" src="images/github.png" alt="gitIcon" />
-			<span>깃허브 로그인</span>
-		</span>
-	</Button>
+<div class="Setting">
+	Setting page
 </div>
 
 <style lang="scss">
-	.Login {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding-top: 3rem;
-
-		&__logo {
-			height: 9.375rem;
-		}
-
-		&__slot {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			gap: 0.3rem;
-		}
-
-		&__icon {
-			width: 1.5rem;
-		}
+	.Setting {
+		
 	}
 </style>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import Home from './Home.svelte';
 import Test from './Test.svelte';
 import Login from './Login.svelte';
 import Setting from './Setting.svelte';
+import ChallengeDetail from './ChallengeDetail.svelte';
 import NotFound from './NotFound.svelte';
 
 export default {
 	'/': Home,
 	'/test' : Test,
 	'/login' : Login,
-	'/:id/setting' : Setting,
+	'/setting/:id' : Setting,
+	'/challenge/:id' : ChallengeDetail,
 	'*': NotFound,
 }
