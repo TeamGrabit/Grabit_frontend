a2ae17bb1822877b135e02864c6ab2abee23aecd
suhong <67825309+tnghd5761@users.noreply.github.com>
2022-01-29 21:46:48 +0900

    Revert "Merge pull request #4 from TeamGrabit/CreateChallenge"
    This reverts commit 103194576685d1d68781829ab8d76d860e5db2b3.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import Home from './Home.svelte';
 import Test from './Test.svelte';
 import Login from './Login.svelte';
 import Setting from './Setting.svelte';
-import CreateChallenge from './CreateChallenge.svelte';
 import ChallengeDetail from './ChallengeDetail.svelte';
 import NotFound from './NotFound.svelte';
 
@@ -11,7 +10,6 @@ export default {
 	'/test' : Test,
 	'/login' : Login,
 	'/setting/:id' : Setting,
-	'/create' : CreateChallenge,
 	'/challenge/:id' : ChallengeDetail,
 	'*': NotFound,
 }
