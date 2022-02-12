b61067f102a3085c4c3034aa60faea4d5f6bb144
suhong <67825309+tnghd5761@users.noreply.github.com>
2022-02-12 22:36:38 +0900

    feat: chat with fixed position
    채팅 버튼 위치 고정 및 onClick 적용

---
+++ src/components/ChatButton.svelte
@@ -0,0 +1,37 @@
+<script>
+	let isClicked = false;
+	function onClick() {
+		isClicked = !isClicked;
+	}
+</script>
+
+{#if isClicked}
+	<div class="chat_system" on:click={onClick}>켜짐</div>
+{:else}
+	<div class="chat_btn" on:click={onClick}>
+		<!--<div class="plus_icon">+</div>-->
+	</div>
+{/if}
+
+<style lang="scss">
+	.chat_system{
+		position: fixed;
+		right: 5vw;
+		bottom: 10vh;
+		width: 10rem;
+		height: 20rem;
+		border: 1.5px solid #DDDDDD;
+	}
+	.chat_btn{
+		position: fixed;
+		right: 5vw;
+		bottom: 10vh;
+		background-color: aquamarine;
+		max-width: 10vw;
+		max-height: 10vw;
+		width: 5rem;
+		height: 5rem;
+		border-radius: 2.5rem;
+		cursor: pointer;
+	}
+</style>
