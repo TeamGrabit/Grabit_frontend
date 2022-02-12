58769179e2405dfe0256769cdd6c3e69978a2662
김수빈 <57705512+MOBUMIN@users.noreply.github.com>
2022-02-12 17:48:33 +0900

    feat: API 요청 함수 생성

--- src/common/fetch.js
+++ src/common/fetch.js
@@ -1,5 +1,15 @@
 const API_URL = 'https://grabit.duckdns.org/api';
 
+/* 사용법
+  fetchPost("/hi", {
+	title: "Test",
+	body: "I am testing!",
+	userId: 1,
+  })
+	.then((data) => console.log(data))
+	// .catch((error) => console.log(error));
+*/
+
 export async function fetchGet(path, options = {}) {
 	const url = `${API_URL}/${path}`;
 
@@ -10,16 +20,16 @@ export async function fetchGet(path, options = {}) {
 
 }
 
-async function fetchPost(path, body, headers = {}) {
+export async function fetchPost(path, body, headers = {}) {
 	const url = `${API_URL}/${path}`;
 
 	const options = {
-	  method: "POST",
-	  headers: {
-		"Content-Type": "application/json",
-		...headers,
-	  },
-	  body: JSON.stringify(body),
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			...headers,
+		},
+		body: JSON.stringify(body),
 	};
 	const res = await fetch(url, options);
 	const data = await res.json();
@@ -35,12 +45,33 @@ async function fetchPost(path, body, headers = {}) {
 	return data;
 }
 
-/* 사용법
-  fetchPost("/hi", {
-	title: "Test",
-	body: "I am testing!",
-	userId: 1,
-  })
-	.then((data) => console.log(data))
-	.catch((error) => console.log(error));
-*/
+export async function fetchPut(path, body, headers = {}) {
+	const url = `${API_URL}/${path}`;
+
+	const options = {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+			...headers,
+		},
+		body: JSON.stringify(body),
+	};
+
+	const res = await fetch(url, options)
+	const data = await res.json();
+
+	return data;
+}
+
+export async function fetchDelete(path) {
+	const url = `${API_URL}/${path}`;
+
+	const options = {
+		method: "DELETE"
+	};
+
+	const res = await fetch(url, options)
+	const data = await res.json();
+
+	return data;
+}
