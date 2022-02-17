2ef8ef9f017c4ecdc9d9bdb76a57ae5cab08e047
llJTOll <eodeoddl315@naver.com>
2022-02-17 18:14:47 +0900

    feat: SearchInput 컴포넌트 추가

--- src/storybook/index.js
+++ src/storybook/index.js
@@ -2,6 +2,7 @@ export { default as Button } from './Button.svelte';
 export { default as Dropdown } from './Dropdown.svelte';
 export { default as DropdownItem } from './DropdownItem.svelte';
 export { default as Inputxt } from './Inputxt.svelte';
+export { default as SearchInput } from './SearchInput.svelte';
 export { default as Loader } from './Loader.svelte';
 export { default as SubNav } from './SubNavigation.svelte';
 export { default as SubNavItem } from './SubNavigationItem.svelte';
