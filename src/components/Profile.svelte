5bee5021327c2171ffcba22903636ebc76c7acc8
suhong <67825309+tnghd5761@users.noreply.github.com>
2022-02-23 16:47:35 +0900

    feat: profile 이미지 연결

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,9 +1,12 @@
 <script>
 	import { user } from '../store/user.js';
+	import { GIT_URL } from '../common/Variable.js';
 </script>
 
 <div class="profile">
-	<div class="profile_img" />
+	{#if $user}
+		<img src='{GIT_URL}/{$user.githubId}.png' alt='userProfile' class="profile_img" />
+	{/if}
 	<div class="profile_id">{$user?.githubId}</div>
 	<div class="edit_btn">Edit profile</div>
 </div>
@@ -12,21 +15,24 @@
 	.profile{
 		position: relative;
 		width: 33%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 
 		.profile_img{
 			border: #AAAAAA solid 2px;
-			background-color: #FAE5E5;
 			border-radius: 50%;
-			width: 90%;
-			padding-bottom: 90%;
+			width: 22.5rem;
+			height: 22.5rem;
 		}
 		.profile_id{
+			width: 22.5rem;
 			padding: 0.625rem 0 0.625rem 0;
 			font-size: 1.2rem;
 			color: #666666;
 		}
 		.edit_btn{
-			width: 90%;
+			width: 22.5rem;
 			height: 1.875rem;
 			padding-top: 0.46875rem;
 			text-align: center;
